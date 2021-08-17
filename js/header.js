84ee9c02d7237789747ffa6f9ac6a78892f8c2e6
misulkin <arthurmisko@gmail.com>
2021-08-17 17:40:33 +0300

    FinishUpdate

--- js/header.js
+++ js/header.js
@@ -119,3 +119,18 @@ $('.testimonial__slider').slick({
 /*$(document).ready(function () {
     $('.products-slider').slick()
 })*/
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault()
+
+        const blockID = anchor.getAttribute('href').substr(1)
+
+        document.getElementById(blockID).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    })
+}
